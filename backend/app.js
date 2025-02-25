89a5ddbe4bb0af176abc95919dfddb3f34d14a10
saranshh <saransh@transvision.us>
2025-02-26 02:37:13 +0530

    cors error fixes

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,14 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path"); 
 const errorMiddleware = require("./middleware/error");
+const cors = reqquire("cors");
  
 //Config
 // if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({path:"backend/config/config.env"});
 // }
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
